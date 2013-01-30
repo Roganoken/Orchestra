821e0cb81620a92cc55a3d53ba631d779507d4ed
maud33800 <nini.maud@gmail.com>
2013-01-30 13:49:29 +0100

    fin bibli + menu gauche

--- src/Orchestra/OrchestraBundle/Extension/MyTwigExtension.php
+++ src/Orchestra/OrchestraBundle/Extension/MyTwigExtension.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Orchestra\OrchestraBundle\Extension;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\httpKernel\KernelInterface;
 
 /**
@@ -34,7 +35,56 @@ class MyTwigExtension extends \Twig_Extension
      */
     public function getName()
     {
-        return 'Array';
+        return 'OrchestraTwigExtensions';
+    }
+    
+    
+    protected $request;
+    /**
+     *
+     * @var \Twig_Environment
+     */
+    protected $environment;
+    
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+    
+    public function initRuntime(\Twig_Environment $environment)
+    {
+        $this->environment = $environment;
+    }
+    
+    public function getFunctions()
+    {
+        return array(
+                'getControllerName' => new \Twig_Function_Method($this, 'getControllerName'),
+                'getActionName' => new \Twig_Function_Method($this, 'getActionName'),
+                
+        );
+    }
+    
+    /**
+     * Get controller name
+     */
+    public function getControllerName()
+    {
+        $regexp = "#Controller\\\([a-zA-Z]*)Controller#";
+        $results = array();
+        preg_match($regexp, $this->request->get('_controller'), $results);
+        return strtolower($results[1]);
+    }
+    
+    /**
+     * Get action name
+     */
+    public function getActionName()
+    {
+        $regexp = "#::([a-zA-Z]*)Action#";
+        $results = array();
+        preg_match($regexp, $this->request->get('_controller'), $results);
+        return $results[1];
     }
 
 /**
